f2f8c768b669d8e84c79025deb8ba65d95b6e075
LuciyR <obraz.org@gmail.com>
2015-11-21 18:53:33 +0200

    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,6 +37,12 @@ class OrdersController < ApplicationController
 	  rescue Stripe::CardError => e
 	    flash[:danger] = e.message
 	  end
+	  
+	  transfer = Stripe::Transfer.create(
+		  :amount    => (@listing.price * 95).floor,
+		  :currency  => "usd",
+		  :recipient => @seller.recipient
+	  )
     
     if @order.save
 	    redirect_to root_url
